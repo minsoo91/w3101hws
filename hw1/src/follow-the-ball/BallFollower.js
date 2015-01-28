aebbc5e44abc15701e6529654769260bff1e57a7
Minsoo <minsoo91@gmail.com>
2015-01-27 17:59:50 -0800

    Pass all specs except the erroneous spec on BallFollowers

--- hw1/src/follow-the-ball/BallFollower.js
+++ hw1/src/follow-the-ball/BallFollower.js
@@ -2,9 +2,18 @@
   'use strict';
 
   var BallFollower = function BallFollower(start, swaps) {
-
+    this.start = start;
+    this.swaps = swaps;
+    this.swap = function () {
+        var currentSwap = this.swaps.shift();
+        var index = currentSwap.indexOf(this.start);
+        if (index > -1) {
+            currentSwap.splice(index, 1);
+            this.start = currentSwap[0];
+        }
+        return this.start;
+    }
   };
-
 
   module.exports = BallFollower; // DON'T ALTER THIS
 })();
